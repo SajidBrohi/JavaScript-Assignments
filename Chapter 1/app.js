acc3122fc5e88535b6888cc7df5c98b6c763824c
SajidBrohi <57886712+SajidBrohi@users.noreply.github.com>
2024-02-29 19:25:28 +0500

    Add files via upload

---
+++ Chapter 1/app.js
@@ -0,0 +1,13 @@
+/*
+// Task 1
+alert('Hello')
+// Task 2
+alert('Javascript Error \n Error! Please enter a valid password.')
+// Task 3
+alert('Welcome to JS Land... \n Happy Coding!')
+// Task 4
+alert('Welcome to JS Land...')
+alert('Happy Coding!')
+// Task 5
+console.log("Hello... I can run JS through my web browser's console")
+*/
