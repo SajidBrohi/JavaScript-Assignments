d2abca9cdb772354dd564ed171153fa091bbc5fa
SajidBrohi <57886712+SajidBrohi@users.noreply.github.com>
2024-02-29 19:26:31 +0500

    Add files via upload

---
+++ Chapter 3/app.js
@@ -0,0 +1,16 @@
+// Task 1
+var age=26;
+alert("I am " + age + " years old");
+// Task 2
+var timesVisited=1;
+for(var timesVisited; timesVisited<15; timesVisited++){
+alert("You have visited this site " + timesVisited + " times");
+}
+// Task 3
+var birthYear=1997;
+alert("My birth year is" + birthYear /n + "Data type of my declared variable is " + typeof(birthYear));
+// Task 4
+var visitorsName='John Doe';
+var productTitle='T-shirt';
+var quantity=5;
+document.write(" <p> <b>visitorsName</b> ordered <b>quantity</b> <b>productTtitle</b>(s) Clothing store</p>")
