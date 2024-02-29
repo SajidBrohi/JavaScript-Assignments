b2ec92877075637cc928282a3a45e14c06bc749e
SajidBrohi <57886712+SajidBrohi@users.noreply.github.com>
2024-02-29 19:26:06 +0500

    Add files via upload

---
+++ Chapter 2/app.js
@@ -0,0 +1,25 @@
+// Task 1
+var username;
+// Task 2
+var userName='Sajid Brohi';
+// Task 3
+var message='Hello World';
+alert(message);
+// Task 4
+var studentName='Jhone Doe';
+var studentAge='15 years old';
+var studentMessage='Certified Mobile Application Developer';
+alert(studentName);
+alert(studentAge);
+alert(studentMessage);
+// Task 6
+var email='example@example.com';
+alert("My email address is " + email );
+// Task 7
+var book="A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book );
+// Task 8
+document.write("<p>Yah! I can write HTML content through JavaScript</p>")
+// Task 9
+var sign= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(sign);
